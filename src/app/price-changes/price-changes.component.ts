7201b764da1d296424aa19652b4d0441c6810a6d
Haseeb <muhammad.haseeb@tritechnologies.com.pk>
2022-08-24 01:53:05 +0500

    setting api integrate

--- src/app/price-changes/price-changes.component.ts
+++ src/app/price-changes/price-changes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,FormBuilder, } from '@angular/forms';
+import { NavigationExtras } from '@angular/router';
+import { API } from 'aws-amplify';
 
 @Component({
   selector: 'app-price-changes',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceChangesComponent implements OnInit {
 
-  constructor() { }
+
+  setting_flag: FormControl;
+  userID: any;
+  constructor() { 
+    this.userID = JSON.parse(localStorage.getItem('UserID')!);
+    this.setting_flag = new FormControl();
+
+  }
 
   ngOnInit(): void {
   }
 
-}
+   sendValue() {
+    this.setting_flag.value;
+    //@ts-ignore
+       API.put('AdAgenzyCRUD', '/items/pricechangerule', {
+      body: {
+        setting_flag: this.setting_flag.value,
+        UserID: this.userID
+      }
+    }).then((value)=>{
+      console.log(value)
+    });
+    
+  }
+  }
+
+
