6fd112c819b343c7d9b4d6dd026079454ddb42bf
Haseeb <muhammad.haseeb@tritechnologies.com.pk>
2022-09-13 18:30:07 +0500

    comit

--- src/app/add-rules/add-rules.component.ts
+++ src/app/add-rules/add-rules.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,FormBuilder, } from '@angular/forms';
+import { NavigationExtras } from '@angular/router';
+import { API } from 'aws-amplify';
 
 @Component({
   selector: 'app-add-rules',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRulesComponent implements OnInit {
 
-  constructor() { }
+  userID: any;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.userID = JSON.parse(localStorage.getItem('UserID')!);
+
+
+    this.form = this.fb.group({
+      rule:[''],
+      priority:[''],
+      setting_flag: [''],
+      strategy_selector: [''],
+      restrictions: [''],
+      stock:[''],
+      rounddownprice:[''],
+      filterproductrules:[''],
+      matchingcriteria:[''],
+      fvtcompetitors:[''],
+      filterbrands:[''],
+      filtercategory:[''],
+
+
+      
+    })
+
+  }
 
   ngOnInit(): void {
+  }
+
+  SaveSettings(){
+    // this.form = this.form.getRawValue();
+     //@ts-ignore
+     API.put('AdAgenzyCRUD', '/items/settings', {
+      body: {
+        ...this.form.getRawValue(),
+
+        UserID: this.userID
+      }
+      
+      
+    }).then((value)=>{
+      console.log(value)
+      console.log();
+    });
   }
 
 }
